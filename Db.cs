3fb7fdc836b9f51af6eddc5da667b5e62408d1ab
Aretha Lemos de Freitas <aretha.lemos@icloud.com>
2022-12-11 10:19:12 -0300

    add: API minimal with EF

--- Db.cs
+++ Db.cs
@@ -1,37 +1,37 @@
 namespace PizzaStore.DB;
 
-public record Pizza
+public record PizzaSample
 {
     public int Id { get; set; }
     public string? Name { get; set; }
 }
 
-public class PizzaDB
+public class PizzaDatabase
 {
-    private static List<Pizza> _pizzas = new List<Pizza>()
+    private static List<PizzaSample> _pizzas = new List<PizzaSample>()
     {
-        new Pizza{ Id=1, Name="Montemagno, Pizza shaped like a great mountain" },
-        new Pizza{ Id=2, Name="The Galloway, Pizza shaped like a submarine, silent but deadly"},
-        new Pizza{ Id=3, Name="The Noring, Pizza shaped like a Viking helmet, where's the mead"}
+        new PizzaSample{ Id=1, Name="Montemagno, Pizza shaped like a great mountain" },
+        new PizzaSample{ Id=2, Name="The Galloway, Pizza shaped like a submarine, silent but deadly"},
+        new PizzaSample{ Id=3, Name="The Noring, Pizza shaped like a Viking helmet, where's the mead"}
     };
 
-    public static List<Pizza> GetPizzas()
+    public static List<PizzaSample> GetPizzas()
     {
         return _pizzas;
     }
 
-    public static Pizza? GetPizza(int id)
+    public static PizzaSample? GetPizza(int id)
     {
         return _pizzas.SingleOrDefault(pizza => pizza.Id == id);
     }
 
-    public static Pizza CreatePizza(Pizza pizza)
+    public static PizzaSample CreatePizza(PizzaSample pizza)
     {
         _pizzas.Add(pizza);
         return pizza;
     }
 
-    public static Pizza UpdatePizza(Pizza update)
+    public static PizzaSample UpdatePizza(PizzaSample update)
     {
         _pizzas = _pizzas.Select(pizza =>
         {

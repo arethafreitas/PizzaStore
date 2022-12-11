46b73dc8e919ea8038abbe8992f6c703d0a450b6
Aretha Lemos de Freitas <aretha.lemos@icloud.com>
2022-12-11 00:45:22 -0300

    add identação

--- Db.cs
+++ Db.cs
@@ -11,8 +11,8 @@ public class PizzaDB
     private static List<Pizza> _pizzas = new List<Pizza>()
     {
         new Pizza{ Id=1, Name="Montemagno, Pizza shaped like a great mountain" },
-     new Pizza{ Id=2, Name="The Galloway, Pizza shaped like a submarine, silent but deadly"},
-     new Pizza{ Id=3, Name="The Noring, Pizza shaped like a Viking helmet, where's the mead"}
+        new Pizza{ Id=2, Name="The Galloway, Pizza shaped like a submarine, silent but deadly"},
+        new Pizza{ Id=3, Name="The Noring, Pizza shaped like a Viking helmet, where's the mead"}
     };
 
     public static List<Pizza> GetPizzas()
